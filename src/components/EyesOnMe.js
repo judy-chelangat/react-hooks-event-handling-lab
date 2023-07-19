a5b8a5c8d0b762fa2ddbb163888416352befb6da
judy-chelangat <judysigilai75@gmail.com>
2023-07-19 17:43:39 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React, { useState } from 'react';
+
+function EyesOnMe(){
+    const [isFocused, setIsFocused] = useState(false);
+
+    function handleFocus(){
+        setIsFocused(true)
+       console.log("true")
+    }
+    function handleBlur(){
+        setIsFocused(false)
+        console.log("eyes on me")
+    }
+    return (
+        <>
+    <button onFocus={handleFocus} onBlur={handleBlur}> Eyes on me</button>
+        </>
+    )
+}
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function Change(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={Change}/>
+        </div>
     )
 }
 
